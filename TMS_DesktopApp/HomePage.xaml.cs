40e783027dbe9c6471f9be18dc308f62b654a987
mefa <mefaba@outlook.com>
2022-11-21 13:29:10 -0500

    Add HomePageView

--- TMS_DesktopApp/HomePage.xaml.cs
+++ TMS_DesktopApp/HomePage.xaml.cs
@@ -1,5 +1,4 @@
-﻿using MySql.Data.MySqlClient;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,15 +16,13 @@ using System.Windows.Shapes;
 namespace TMS_DesktopApp
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for HomePage.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class HomePage : Page
     {
-        public MainWindow()
+        public HomePage()
         {
-            //testing something
             InitializeComponent();
-            MainFrame.Content = new HomePage();
         }
     }
 }
