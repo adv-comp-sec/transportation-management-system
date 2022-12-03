3a2ba1f6b648b1c24d9ac52c9c39c268a75aa2b5
mefa <mefaba@outlook.com>
2022-12-03 01:01:05 -0500

    Working MarketPage

--- TMS_DesktopApp/BuyerView/MarketPage.xaml.cs
+++ TMS_DesktopApp/BuyerView/MarketPage.xaml.cs
@@ -7,6 +7,7 @@ using System.Collections.ObjectModel;
 using System.Data;
 using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
@@ -44,42 +45,18 @@ namespace TMS_DesktopApp
         public MarketPage()
         {
             InitializeComponent();
+            
             PopulateTable();
         }
         DataSet ds = new DataSet();
         public void PopulateTable()
         {
-        
             //MySqlConnection conn = new MySqlConnection("Server=159.89.117.198;Port=3306;user=DevOSHT;password=Snodgr4ss!;Database=cmp;");
             MySqlConnection conn = new DataAccess().ConnectMarketDB();
-           // MySqlCommand cmd = new MySqlCommand("SELECT * FROM Contract;",conn);
-
+            // MySqlCommand cmd = new MySqlCommand("SELECT * FROM Contract;",conn);
             MySqlDataAdapter da = new MySqlDataAdapter("SELECT * FROM Contract;", conn);
-            MySqlCommandBuilder cb = new MySqlCommandBuilder(da);
             da.Fill(ds);
             marketDBTable.ItemsSource = ds.Tables[0].AsDataView();
-            
-            /*conn.Open();
-            MySqlDataReader rdr = cmd.ExecuteReader();
-            
-            while (rdr.Read())
-            {
-                Contract newContract = new Contract
-                {
-                    Client_Name = rdr.GetString(0),
-                    Job_Type = rdr.GetString(1),
-                    Quantity = rdr.GetString(2),
-                    Origin = rdr.GetString(3),
-                    Destination = rdr.GetString(4),
-                    Van_Type = rdr.GetString(5),
-                };
-                //AddItem(newContract);
-                
-
-            }
-            marketDBTable.ItemsSource = MarketData;
-            rdr.Close();
-            conn.Close();*/
         }
 
         public void AddItem(Contract newContract)
@@ -88,58 +65,36 @@ namespace TMS_DesktopApp
             MarketData.Add(newContract);
         }
 
-        public void RemoveItem(int index)
-        {
-            marketDBTable.Items.RemoveAt(index);
-            MarketData.RemoveAt(index);
-        }
-
-        public void ClearMarket()
-        {
-            marketDBTable.Items.Clear();
-            MarketData = new List<Contract>() { };
-        }
-
         private void marketDBTable_SelectedCellsChanged(object sender, SelectedCellsChangedEventArgs e)
         {
             //marketDBTable.IsEnabled = false;
             string selected = marketDBTable.SelectedIndex.ToString();
             var row = marketDBTable.SelectedItems;
-            
         }
 
         private void btn_Refresh_Click(object sender, RoutedEventArgs e)
         {
-            var index = marketDBTable.SelectedIndex;
-            var sItem = marketDBTable.SelectedItem;
-            var client = ds.Tables[0].Rows[index][0];
-            ds.Tables[0].Rows[index][0] = "Fatih";
-            //var row = sItem.
-            //var something = data.Row[0];
-            //var client = data.it;
-            /*ClearMarket();
-            PopulateTable();*/
+            ds.Clear();
+            PopulateTable();
         }
 
         private void btn_CreateOrder_Click(object sender, RoutedEventArgs e)
         {
 
             //GET VALUES OF ORDER
-            var selectedContract = MarketData[marketDBTable.SelectedIndex];
-            RemoveItem(marketDBTable.SelectedIndex);
-            /*IS Client is a customer of TMS?*/
+            var selectedContract = ds.Tables[0].Rows[marketDBTable.SelectedIndex].ItemArray;
+            //Remove selected row
+            ds.Tables[0].Rows.RemoveAt(marketDBTable.SelectedIndex);
+            /*TODO: IS Client is a customer of TMS?*/
             /*SUCCESS*/
             //add order to database
             MySqlConnection conn = new DataAccess().ConnectTmsDB();
-
             string cmdText = $"INSERT INTO orders(CustomerID,TripID ,RelevantCitiesId,OrderStatus,OrderDate,Origin,Destination,VanType,JobType,Quantity) " +
-                $"VALUES (1,2,1,'Recieved','{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}','{selectedContract.Origin}','{selectedContract.Destination}',{selectedContract.Van_Type},{selectedContract.Job_Type},{selectedContract.Quantity});";
+                $"VALUES (1,2,1,'Recieved','{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")}','{selectedContract[3]}','{selectedContract[4]}',{selectedContract[5]},{selectedContract[1]},{selectedContract[2]});";
             MySqlCommand cmd = new MySqlCommand(cmdText, conn);
             conn.Open();
             cmd.ExecuteNonQuery();
             conn.Close();
-            //remove item from table
-               
         }
 
    
